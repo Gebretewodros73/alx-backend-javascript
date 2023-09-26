6f332c416ed651b150688860ca7d29cbf404d1c8
gebreteworos73 <gebreteworos73@gmail.com>
2023-09-26 05:17:46 +0300

    Modify variables inside the taskBlock function so that they are not overwritten inside the conditional block.

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false; // Use const for variables that won't be reassigned
-  let task2 = true;   // Use let for variables that may be reassigned
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // Use const for variables in the block scope
-    task2 = false;     // Reassign task2 with let
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
