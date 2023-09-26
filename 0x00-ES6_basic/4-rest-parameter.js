d50615ff47c946e486abb42c3c1ef448d8998891
gebreteworos73 <gebreteworos73@gmail.com>
2023-09-26 05:37:28 +0300

    Modify the returnHowManyArguments function to return the number of arguments passed to it using the rest parameter syntax.

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,5 @@
+// Define a function named returnHowManyArguments using the rest parameter syntax (...args)
+export default function returnHowManyArguments(...args) {
+  // Return the length of the arguments array, which indicates the number of arguments passed
+  return args.length;
+}
