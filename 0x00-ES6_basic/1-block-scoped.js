d0f0e8f6955d844c5058186df04504e395eed0dd
gebreteworos73 <gebreteworos73@gmail.com>
2023-09-26 05:10:32 +0300

    Modify variables inside the taskBlock function so that they are not overwritten inside the conditional block.

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-	const task = false; // Declared as block-scoped variable
-	const task2 = true; // Declared as block-scoped variable
-	
+	const task = false;
+	const task2 = true;
+
 	if (trueOrFalse) {
-		const task = true; // Declared as block-scoped variable (different from the one above)
-		const task2 = false; // Declared as block-scoped variable (different from the one above)
-		}
+		const task = true;
+		const task2 = false;
+	}
 
 	return [task, task2];
 }
