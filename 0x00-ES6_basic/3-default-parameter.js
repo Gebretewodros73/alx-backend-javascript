e1a326f58120c78522856930ea10370c692a8f90
gebreteworos73 <gebreteworos73@gmail.com>
2023-09-26 05:34:56 +0300

    Condense the internals of the getSumOfHoods function to 1 line using default parameter values.

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,6 +1,3 @@
-// Define the function getSumOfHoods with three parameters: initialNumber, expansion1989, and expansion2019
-// Set default values for expansion1989 and expansion2019 to 89 and 19 respectively
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-  // Return the sum of initialNumber, expansion1989, and expansion2019
   return initialNumber + expansion1989 + expansion2019;
 }
