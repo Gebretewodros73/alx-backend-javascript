6a8cd83fd4fe16eaf080c29bf3e28261e4b57b91
gebreteworos73 <gebreteworos73@gmail.com>
2023-09-27 05:48:36 +0300

    Import functions `uploadPhoto` and `createUser` to collectively resolve promises and log user information.

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+  const promises = [uploadPhoto(), createUser()];
+
+  return Promise.all(promises)
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
