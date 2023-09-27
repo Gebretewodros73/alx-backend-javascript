819a431afadb64e5216bb50e9d539ea215b2b6c0
gebretewodros73 <gebretewodros73@gmail.com>
2023-09-27 04:40:47 +0300

    Create a Promise using the function `getResponseFromAPI()`.

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulate an API call
+    setTimeout(() => {
+      const data = { message: 'API response data' };
+      resolve(data);
+    }, 2000); // Assuming a 2-second delay for the API call
+  });
+}
+
+export default getResponseFromAPI;
+
