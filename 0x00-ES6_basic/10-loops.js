e85ca7b69d6fc3cf07ac1ddbcb9d7df6a1f01b42
gebretewodros73 <gebretewodros73@gmail.com>
2023-09-28 15:43:08 +0300

    eslint --fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,11 +1,12 @@
 // Define a function named appendToEachArrayValue with two parameters: array and appendString
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
   // Use a for...of loop to iterate through the elements of the array
   for (const value of array) {
     // Append the appendString to each element of the array
-    value = appendString + value; // Note: This line will throw an error, so it's commented out
+    newArray.push(appendString + value);
   }
 
   // Return the modified array
-  return array;
+  return newArray;
 }
