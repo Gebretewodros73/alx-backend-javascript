ff2c07488b4447c4a3a56a8d192bf88896bd428d
gebretewodros73 <gebretewodros73@gmail.com>
2023-09-28 03:17:08 +0300

    eslint --fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   const promises = [uploadPhoto(), createUser()];
