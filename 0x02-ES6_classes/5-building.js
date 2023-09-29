2913a6c4368156999b8940d892967e1554925119
gebreteworos73 <gebreteworos73@gmail.com>
2023-09-29 23:53:10 +0300

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,39 +1,20 @@
-/**
- * Represents a Building.
- */
 export default class Building {
-  /**
-   * Creates a new Building.
-   * @param {Number} sqft - The square footage.
-   */
   constructor(sqft) {
     this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
   }
 
-  /**
-   * Gets the square footage.
-   * @returns {Number} The square footage.
-   */
   get sqft() {
     return this._sqft;
   }
 
-  /**
-   * Sets the square footage.
-   * @param {Number} value - The square footage.
-   */
   set sqft(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Square footage must be a number');
-    }
     this._sqft = value;
-  }
-
-  /**
-   * Provides an evacuation warning message.
-   * @throws {Error} Throws an error if not overridden by subclass.
-   */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
