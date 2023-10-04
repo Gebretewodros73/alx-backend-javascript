e18d64a36c8c22bc95a5c16b6024aee9304f2de3
gebreteworos73 <gebreteworos73@gmail.com>
2023-10-04 04:59:25 +0300

    fix the output

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,32 @@
-function cleanSet(set, startString) {
-  let result = '';
+/**
+ * Joins a set of strings with a dash after stripping the strings of
+ * a leading sub string.
+ *
+ * @param {Set<String>} set - A collection of strings.
+ * @param {String} startString - The string to strip from the beginning
+ * of each item in the set.
+ *
+ * @returns {String} - A hyphen-separated string of modified values.
+ */
+export default function cleanSet(set, startString) {
+  const parts = []; // Initialize an array to store modified strings.
 
-  // Iterate through the elements of the Set
-  for (const element of set) {
-    // Check if the element starts with startString
-    if (element.startsWith(startString)) {
-      // Append the rest of the string to the result
-      result += `${element.slice(startString.length)}-`;
+  // Check if set or startString are invalid, return empty string.
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+
+  // Iterate over the values in the set.
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
     }
   }
 
-  // Remove the trailing '-' if it exists
-  if (result.endsWith('-')) {
-    result = result.slice(0, -1);
-  }
-
-  return result;
+  // Join the modified strings with a dash and return the result.
+  return parts.join('-');
 }
-
-export default cleanSet;
