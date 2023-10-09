80b1d5ab337d80c185651f4fa6aeba66a3ee66e7
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-09 04:21:44 +0300

    accepts two arguments firstName and lastName and returns the first letter of the firstName and the full lastName

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,3 +1,19 @@
+var teacher1 = {
+    firstName: "John",
+    lastName: "Doe",
+    fullTimeEmployee: true,
+    yearsOfExperience: 5,
+    location: "New York"
+};
+var teacher2 = {
+    firstName: "Jane",
+    lastName: "Smith",
+    fullTimeEmployee: false,
+    location: "Los Angeles",
+    contract: true // Additional attribute
+};
+console.log(teacher1);
+console.log(teacher2);
 var director1 = {
     firstName: 'John',
     lastName: 'Doe',
@@ -15,3 +31,12 @@ var director2 = {
 };
 console.log(director1);
 console.log(director2);
+// Task 3: Printing teachers
+function printTeacher(firstName, lastName) {
+    var firstLetter = firstName.charAt(0);
+    return "".concat(firstLetter, ". ").concat(lastName);
+}
+var teacherName1 = printTeacher(teacher1.firstName, teacher1.lastName);
+var teacherName2 = printTeacher(teacher2.firstName, teacher2.lastName);
+console.log(teacherName1); // Output: J. Doe
+console.log(teacherName2); // Output: J. Smith
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Task 1: Teacher Interface
 interface Teacher {
   firstName: string;
   lastName: string;
@@ -7,6 +8,26 @@ interface Teacher {
   [key: string]: any; // Allow additional attributes
 }
 
+const teacher1: Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  fullTimeEmployee: true,
+  yearsOfExperience: 5,
+  location: "New York"
+};
+
+const teacher2: Teacher = {
+  firstName: "Jane",
+  lastName: "Smith",
+  fullTimeEmployee: false,
+  location: "Los Angeles",
+  contract: true // Additional attribute
+};
+
+console.log(teacher1);
+console.log(teacher2);
+
+// Task 2: Extending Teacher Interface
 interface Directors extends Teacher {
   numberOfReports: number;
 }
@@ -30,3 +51,15 @@ const director2: Directors = {
 
 console.log(director1);
 console.log(director2);
+
+// Task 3: Printing teachers
+function printTeacher(firstName: string, lastName: string): string {
+  const firstLetter = firstName.charAt(0);
+  return `${firstLetter}. ${lastName}`;
+}
+
+const teacherName1 = printTeacher(teacher1.firstName, teacher1.lastName);
+const teacherName2 = printTeacher(teacher2.firstName, teacher2.lastName);
+
+console.log(teacherName1); // Output: J. Doe
+console.log(teacherName2); // Output: J. Smith
