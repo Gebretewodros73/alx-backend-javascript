0cec98b26c7136dff5158b52a4c69e9ba606169b
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-10 05:37:06 +0300

    String literal types

--- 0x04-TypeScript/task_2/js/main.js
+++ 0x04-TypeScript/task_2/js/main.js
@@ -1,30 +1,68 @@
+// Task 5: Advanced types Part 1
+// Classes
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return 'Working from home';
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return 'Getting a coffee break';
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return 'Getting to director tasks';
+    };
+    return Director;
+}());
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return 'Cannot work from home';
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return 'Cannot have a break';
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return 'Getting to work';
+    };
+    return Teacher;
+}());
+// Function for Task 6
+function createEmployee(salary) {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    else {
+        return new Director();
+    }
+}
+// Task 6: Creating functions specific to employees
 function isDirector(employee) {
-    return 'numberOfReports' in employee;
+    return 'workDirectorTasks' in employee;
 }
 function executeWork(employee) {
     if (isDirector(employee)) {
-        return employee.workDirectorTasks();
+        console.log(employee.workDirectorTasks());
     }
     else {
-        return employee.workTeacherTasks();
+        console.log(employee.workTeacherTasks());
     }
 }
-// Example Usage
-var employee1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    fullTimeEmployee: true,
-    yearsOfExperience: 5,
-    location: 'New York',
-    workTeacherTasks: function () { return 'Getting to work'; }
-};
-var employee2 = {
-    firstName: 'Jane',
-    lastName: 'Smith',
-    fullTimeEmployee: false,
-    location: 'Los Angeles',
-    numberOfReports: 10,
-    workDirectorTasks: function () { return 'Getting to director tasks'; }
-};
-console.log(executeWork(employee1)); // Output: Getting to work
-console.log(executeWork(employee2)); // Output: Getting to director tasks
+function teachClass(todayClass) {
+    return todayClass === 'Math'
+        ? 'Teaching Math'
+        : todayClass === 'History'
+            ? 'Teaching History'
+            : 'Unknown subject';
+}
+// Testing the functions
+console.log(createEmployee(200)); // Teacher
+console.log(createEmployee(1000)); // Director
+console.log(createEmployee('$500')); // Director
+var mathClass = teachClass('Math'); // Teaching Math
+var historyClass = teachClass('History'); // Teaching History
+executeWork(createEmployee(200)); // Getting to work
+executeWork(createEmployee(1000)); // Getting to director tasks
+console.log(mathClass);
+console.log(historyClass);
--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,51 +1,86 @@
-interface Director {
-  firstName: string;
-  lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  numberOfReports: number;
+// Task 5: Advanced types Part 1
+
+// Interfaces
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
 }
 
-interface Teacher {
-  firstName: string;
-  lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
 }
 
-function isDirector(employee: Director | Teacher): employee is Director {
-  return 'numberOfReports' in employee;
-}
-
-function executeWork(employee: Director | Teacher): string {
-  if (isDirector(employee)) {
-    return employee.workDirectorTasks();
-  } else {
-    return employee.workTeacherTasks();
+// Classes
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
   }
 }
 
-// Example Usage
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
 
-const employee1: Teacher = {
-  firstName: 'John',
-  lastName: 'Doe',
-  fullTimeEmployee: true,
-  yearsOfExperience: 5,
-  location: 'New York',
-  workTeacherTasks: () => 'Getting to work'
-};
+// Function for Task 6
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
 
-const employee2: Director = {
-  firstName: 'Jane',
-  lastName: 'Smith',
-  fullTimeEmployee: false,
-  location: 'Los Angeles',
-  numberOfReports: 10,
-  workDirectorTasks: () => 'Getting to director tasks'
-};
+// Task 6: Creating functions specific to employees
+function isDirector(employee: Director | Teacher): employee is Director {
+  return 'workDirectorTasks' in employee;
+}
 
-console.log(executeWork(employee1)); // Output: Getting to work
-console.log(executeWork(employee2)); // Output: Getting to director tasks
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+// Task 7: String literal types
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  return todayClass === 'Math'
+    ? 'Teaching Math'
+    : todayClass === 'History'
+    ? 'Teaching History'
+    : 'Unknown subject';
+}
+
+// Testing the functions
+console.log(createEmployee(200)); // Teacher
+console.log(createEmployee(1000)); // Director
+console.log(createEmployee('$500')); // Director
+
+const mathClass = teachClass('Math'); // Teaching Math
+const historyClass = teachClass('History'); // Teaching History
+
+executeWork(createEmployee(200)); // Getting to work
+executeWork(createEmployee(1000)); // Getting to director tasks
+
+console.log(mathClass);
+console.log(historyClass);
